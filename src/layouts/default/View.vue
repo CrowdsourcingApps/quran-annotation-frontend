966bf73f3836a99ba053a516316e8a4d78fbfe6c
raghadsalameh1 <90514155+raghadsalameh1@users.noreply.github.com>
2023-03-02 12:27:03 +0300

    adding dist

--- src/layouts/default/View.vue
+++
@@ -1,9 +0,0 @@
-<template>
-   <v-main :style="$i18n.locale === 'AR' ? 'height: 500px; direction: rtl;' : 'height: 500px;'">
-      <router-view/>
-    </v-main>
-</template>
-
-<script setup>
-
-</script>
