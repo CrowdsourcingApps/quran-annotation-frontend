5bbd9a2a9a6a4f1a01fd18fd4d8f829788ccffe9
raghadsalameh1 <raghadgithub@gmail.com>
2023-09-03 14:35:49 +0300

    store token for authorized users (#37)

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,33 @@
 <script setup>
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import NotificationService from "@/services/notification.service";
 
 const firebaseConfig =JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = getMessaging();
+
 // Get registration token. Initially this makes a network call, once retrieved
 // subsequent calls to getToken will return from cache.
-const messaging = getMessaging();
-onMessage(messaging, (payload) => {
-  console.log('Message received. ', payload);
-  // ...
-});
 getToken(messaging, { vapidKey: import.meta.env.VITE_FIREBASE_API_KEY}).then((currentToken) => {
   if (currentToken) {
-    // Send the token to your server and update the UI if necessary
+    // Check if the user has an account on our website.
+    const loggedIn = localStorage.getItem('user');
+    if(loggedIn){
+      console.log("yes! logged in")
+      NotificationService.store_token(currentToken).then(
+      (response) => {
+         console.log(response)
+      },
+      (error) => {
+          console.log(error);
+      }
+      );
+    }
+    // if the cache is empty -> create anonymuse user & save id in cache -> store token in server & update the cache
+
     // ...
     console.log("Notification token is: ",currentToken)
   } else {
@@ -30,4 +43,14 @@ getToken(messaging, { vapidKey: import.meta.env.VITE_FIREBASE_API_KEY}).then((cu
   console.log('An error occurred while retrieving token. ', err);
   // ...
 });
+
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload);
+  // ...
+});
+
+// Updating Registration Tokens
+// To ensure that a device’s registration token is fresh, you should periodically retrieve and update all existing registration tokens. 
+// To do so, you’ll need to add app logic in the client app to retrieve the current token, and then send the token along with a 
+// timestamp to the server to store. This could be a monthly job.
 </script>
