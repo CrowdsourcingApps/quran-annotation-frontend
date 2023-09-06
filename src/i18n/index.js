e078748792f428191ef04379cb801550116c6fff
raghadsalameh1 <raghadgithub@gmail.com>
2023-09-06 18:35:10 +0300

    Set default language to browser language (#37)

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,11 +3,23 @@ import AR from './locales/ar.json'
 import EN from './locales/en.json'
 import RU from './locales/ru.json'
 
+const availableLocales = ['AR', 'EN', 'RU'];
+
+let initialLocale = localStorage.getItem('userLocale');
+if (!initialLocale) {
+  var browserLanguage = window.navigator.language; 
+  browserLanguage = browserLanguage.split('-')[0].toUpperCase()
+  if(availableLocales.includes(browserLanguage))
+    initialLocale = browserLanguage
+  else
+    initialLocale = 'EN'
+}
+
 const i18n = createI18n({
     legacy: false,
     globalInjection: true,
-    availableLocales: ['AR', 'EN', 'RU'],
-    locale: 'AR',
+    availableLocales: availableLocales,
+    locale: initialLocale,
     fallbackLocale: 'EN',
     messages: {EN,AR,RU},
   });
