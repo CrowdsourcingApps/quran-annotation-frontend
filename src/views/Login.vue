077c44922809ba4f8c43e143f4dd9dc7611057d1
raghadsalameh1 <raghadgithub@gmail.com>
2024-01-19 02:18:41 +0300

    fix logo size in login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,7 +6,7 @@
         <p class="font-weight-bold text-h4 ma-2 pa-2">
           {{ isLoginMode? $t('nav.login') : $t('login.create') }}
         </p>
-        <img class="ml-5 mr-5" src="@/assets/logo.svg" />
+        <img class="ml-5 mr-5" src="@/assets/logo.svg" height="100" width="100" />
         <p class="font-weight-bold text-h6 ma-2 pa-2">{{ $t('homepage.goal') }}</p>
         <v-spacer></v-spacer>
         <v-alert
